ea261bf7396d783631e3269c64f1f2cc3423a718
Tean Concio <tean_concio@dlsu.edu.ph>
2023-12-05 15:50:37 +0800

    Uwu

--- client/ClientClass.java
+++ client/ClientClass.java
@@ -1,7 +1,5 @@
 package client;
 
-import server.UserClass;
-
 import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -70,7 +68,7 @@ public class ClientClass {
             System.out.print("Enter a command: ");
             String command[] = initCommand.split(" ");
 
-            String mainCommand = command[0];
+            //String mainCommand = command[0];
             String ipAddress = command[1];
             int nPort = Integer.parseInt(command[2]);
 
@@ -106,9 +104,9 @@ public class ClientClass {
 
     public void checkRegister (String regCommand) {
         try {
-            String command[] = regCommand.split(" ");
+            //String command[] = regCommand.split(" ");
 
-            String mainCommand = command[0];
+            //String mainCommand = command[0];
 
             Name = registerUser(regCommand);
 
@@ -205,7 +203,7 @@ public class ClientClass {
                 stringAppend = "User handle " + Name + " registered successfully!\n";
                 break;
             case "USER HANDLE ALREADY TAKEN":
-                stringAppend = "That username is already taken. Please select another usrename...\n";
+                stringAppend = "That username is already taken. Please select another username...\n";
                 break;
             case "NOT REGISTERED":
                 stringAppend = "This command is only available to registered users. Please register first.\n";
